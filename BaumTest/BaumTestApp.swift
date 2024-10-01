9cb98fdd9e292d2f002ae171573046316fe18879
emiri0414 <emiri060414@gmail.com>
2024-10-01 18:22:38 +0900

    Initial Commit

---
+++ BaumTest/BaumTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaumTestApp.swift
+//  BaumTest
+//
+//  Created by 中森えみり on 2024/10/01.
+//
+
+import SwiftUI
+
+@main
+struct BaumTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaumTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BaumTest
+//
+//  Created by 中森えみり on 2024/10/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
